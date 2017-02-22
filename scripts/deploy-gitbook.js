24434e3c35238fe1b35a0c0f04cda0726523516d
Jorge Sierra <jsierra.siete@gmail.com>
2017-02-22 14:15:16 -0800

    Removed help

--- scripts/deploy-gitbook.js
+++ scripts/deploy-gitbook.js
@@ -3,8 +3,6 @@ var ghpages = require('gh-pages');
 var path    = require('path');
 var json    = require('../package.json');
 
-path.join(__dirname, 'primeros-pasos-en-nodejs-victor-jesus-pamela')
-
 ghpages.publish(path.join(__dirname, '/../txt') /* Directorio de los src files */, {
     /* Default: branch: 'gh-pages', */
     repo:     json.repository.url,
