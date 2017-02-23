f77435e61e9b6820e2f22ca55f47d65cfb7348d3
Jorge Sierra <jsierra.siete@gmail.com>
2017-02-23 00:53:09 +0000

    Despliege listo

--- hello.js
+++ hello.js
@@ -3,38 +3,15 @@ var app = express()
 var path = require('path');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/gh-pages'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/gh-pages')));
 
 app.get('/', function (req, res) {
   //res.send('Hello World!')
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Gitbook' });
 })
-
-/*
- var router = express.Router();
-  module.exports = router;
-*/
-app.get('/chuchu', function (req, res) {
-  //res.send('Hello Chuchu!')
-  res.render('index', { title: 'Chuchu' });
-})
-
-app.get('/cat', function (req, res) {
-  res.send('Got a GET request'+
-    '<br/><img src="kitten.jpg" />'
-  );
-})
-
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/dog.jpg'));
-});
-
-app.get('/dog', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/dog.jpg'));
-});
 
 var server = app.listen(8080, function() {
 
